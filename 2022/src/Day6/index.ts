d2b03a0c8b2ab7f0bdbba71a2896d43fecce99e3
Jacob Fenton <jjf@copp.com>
2022-12-06 08:50:31 -0500

    Refactor: 2022 Day 6

--- 2022/src/Day6/index.ts
+++ 2022/src/Day6/index.ts
@@ -5,7 +5,7 @@ const data = readFileSync("2022/Inputs/day6.txt").toString().split("")
 function getMarker(ps = 4) {
     const markerIdx = data.findIndex((_,i) => {
         const window = data.slice(i, i + ps)
-        return [...new Set(window)].length === window.length
+        return new Set(window).size === window.length
     })
 
     return markerIdx + ps
